44feddc30cacfdbad545c44591629f94048c05e1
viswanr <viswanath@outlook.com>
2016-04-13 20:24:12 -0700

    Removed SharedView as it is not the right portable project. Will add back later

--- Source/Epiphany.WP8/Services/ViewModelLocator.cs
+++ Source/Epiphany.WP8/Services/ViewModelLocator.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using Epiphany.ViewModel;
+﻿using Epiphany.ViewModel;
 
 namespace Epiphany.View.Services
 {
@@ -12,7 +11,7 @@ namespace Epiphany.View.Services
             if (System.ComponentModel.DesignerProperties.IsInDesignTool)
             {
                 // Use the DesignViewModelLocator for blendability
-                this.locatorImpl = new DesignTimeViewModelLocator();
+                //this.locatorImpl = new DesignTimeViewModelLocator();
             }
             else
             {
