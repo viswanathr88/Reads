eca574d73d643fcbb592ccef33e68faf4e122197
viswanr <viswanath@outlook.com>
2016-04-13 18:06:57 -0700

    Introduced a SharedView project that will be shared between 8.1 and Universal projects.

---
+++ Source/Epiphany.SharedView/Services/SettingStorage.cs
@@ -0,0 +1,56 @@
+﻿using Epiphany.Logging;
+using Epiphany.Model.Settings;
+using System;
+using Windows.Storage;
+
+namespace Epiphany.View.Services
+{
+    public class SettingStorage : ISettingStorage
+    {
+        private readonly object syncRoot = new object();
+
+        public bool AddOrUpdate(string key, object value)
+        {
+            bool valueUpdated = false;
+
+            lock (syncRoot)
+            {
+                try
+                {
+                    if (!ApplicationData.Current.LocalSettings.Values.ContainsKey(key) ||
+                        ApplicationData.Current.LocalSettings.Values[key] != value)
+                    {
+                        ApplicationData.Current.LocalSettings.Values[key] = value;
+                        valueUpdated = true;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Logger.LogException(ex);
+                    valueUpdated = false;
+                }
+            }
+
+            return valueUpdated;
+        }
+
+        public T GetValueOrDefault<T>(string key, T defaultValue)
+        {
+            T value = defaultValue;
+
+            try
+            {
+                if (ApplicationData.Current.LocalSettings.Values.ContainsKey(key))
+                {
+                    value = (T)ApplicationData.Current.LocalSettings.Values[key];
+                }
+            }
+            catch (Exception ex)
+            {
+                Logger.LogException(ex);
+                value = defaultValue; // Just in case, reset it to defaultValue
+            }
+            return value;
+        }
+    }
+}
---
+++ Source/Epiphany.SharedView/Services/Timer.cs
@@ -0,0 +1,62 @@
+﻿using Epiphany.ViewModel.Services;
+using System;
+using Windows.UI.Xaml;
+
+namespace Epiphany.View.Services
+{
+    class Timer : ITimer
+    {
+        private readonly Action tickAction;
+        private readonly DispatcherTimer internalTimer;
+
+        public Timer(Action tickAction)
+        {
+            this.tickAction = tickAction;
+            this.internalTimer = new DispatcherTimer();
+            this.internalTimer.Tick += OnTimeout;
+        }
+
+        public TimeSpan Interval
+        {
+            get
+            {
+                return this.internalTimer.Interval;
+            }
+            set
+            {
+                this.internalTimer.Interval = value;
+            }
+        }
+
+        public bool IsEnabled
+        {
+            get
+            {
+                return this.internalTimer.IsEnabled;
+            }
+        }
+
+        public void Start()
+        {
+            this.internalTimer.Start();
+        }
+
+        public void Stop()
+        {
+            this.internalTimer.Stop();
+        }
+
+        private void OnTimeout(object sender, object e)
+        {
+            if (tickAction != null)
+            {
+                tickAction.Invoke();
+            }
+        }
+
+        public void Dispose()
+        {
+            this.internalTimer.Tick -= OnTimeout;
+        }
+    }
+}
