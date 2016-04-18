2ad718a69c746dfa82bf97419e6fbcefd8abffd3
Viswanath Ramanan <viswanath@outlook.com>
2016-04-17 23:59:53 -0700

    Missed 2 files before.

---
+++ Source/Epiphany.Model/Web/IAuthenticatorFactory.cs
@@ -0,0 +1,14 @@
+﻿namespace Epiphany.Web
+{
+    /// <summary>
+    /// Factory interface for creating different authenticators
+    /// </summary>
+    public interface IAuthenticatorFactory
+    {
+        /// <summary>
+        /// Creates an oauth authenticator
+        /// /// </summary>
+        /// <returns></returns>
+        IAuthenticator CreateOAuthAuthenticator();
+    }
+}
---
+++ Source/Epiphany.Model/Web/WebExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading.Tasks;
+
+namespace Epiphany.Web
+{
+    /// <summary>
+    /// Extensions to web
+    /// </summary>
+    public static class WebExtensions
+    {
+        /// <summary>
+        /// Gets the response async from System.Net.WebRequest
+        /// </summary>
+        /// <param name="request"></param>
+        /// <returns></returns>
+        public static Task<System.Net.WebResponse> GetResponseAsync(this System.Net.WebRequest request)
+        {
+            return Task.Factory.FromAsync<System.Net.WebResponse>(
+                request.BeginGetResponse, request.EndGetResponse, null);
+        }
+    }
+}
